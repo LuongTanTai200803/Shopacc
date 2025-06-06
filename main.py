977fca94ed531bec75c2f0a8a6e3bc14c0e74b02
Tai Dev <tantailg2003@gmail.com>
2025-06-06 16:44:08 +0700

    change config_class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from app import create_app, setup_logging
 from app.config import Testing, Production, Config
 
 setup_logging()
-app = create_app(config_class=Testing)
+app = create_app(config_class=Production)
 
 if __name__ == '__main__':
     app.run(debug=False)

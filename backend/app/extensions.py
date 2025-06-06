8b998cc1be22b7dca00f3cf4382300b55b8855ad
Tai Dev <tantailg2003@gmail.com>
2025-06-06 15:23:01 +0700

    remove ../

--- backend/app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-jwt= JWTManager()
-migratie = Migrate()

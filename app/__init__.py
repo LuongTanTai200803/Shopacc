c1d4947bae5242e6a2a54053eb80983d270ea6f1
Tai Dev <tantailg2003@gmail.com>
2025-06-08 01:36:06 +0700

    Fix deploy v2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 import os
 import logging
+import time
+
 from flask import Flask
 from flask_migrate import upgrade
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
 from app.config import Config
 from app.extensions import db, jwt, migratie
 from app.log_request import setup_request_logger
@@ -11,53 +15,61 @@ from flask_cors import CORS
 
 
 def create_app(config_class = Config):
-    app = Flask(__name__)
+    try:
+        app = Flask(__name__)
 
-    # Cấu hình CORS với nguồn gốc cụ thể
-    CORS(app, resources={r"/*": {
-        "origins": ["https://shopacc22222-production.up.railway.app"],
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": ["Content-Type", "Authorization"]
-    }})
+        # Cấu hình CORS với nguồn gốc cụ thể
+        CORS(app, resources={r"/*": {
+            "origins": ["https://shopacc22222-production.up.railway.app"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"]
+        }})
 
-    app.config.from_object(config_class)
+        app.config.from_object(config_class)
 
-    db.init_app(app)
-    jwt.init_app(app)
-    migratie.init_app(app, db)
+        db.init_app(app)
+        jwt.init_app(app)
+        migratie.init_app(app, db)
 
-    from app.models import User, Acc 
-    with app.app_context():
-        # Tự động chạy migration
-        upgrade()
-        
-    # Đăng ký blueprint
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(acc_bp, url_prefix="/acc")
-    app.register_blueprint(order_bp, url_prefix="/order")
+        from app.models import User, Acc 
+        with app.app_context():
+            print("Running DB migrations...")
+            upgrade()
+            print("DB migrations completed.")
+            
+        # Đăng ký blueprint
+        app.register_blueprint(auth_bp, url_prefix="/auth")
+        app.register_blueprint(acc_bp, url_prefix="/acc")
+        app.register_blueprint(order_bp, url_prefix="/order")
 
-    # Đăng ký log
-    setup_request_logger(app)
-    # Đăng ký error handler
-    register_error_handlers(app)
+        # Đăng ký log
+        setup_request_logger(app)
+        # Đăng ký error handler
+        register_error_handlers(app)
 
-    """ from app.models.user import User
-    print(User.__table__.columns.keys()) """
+        """ from app.models.user import User
+        print(User.__table__.columns.keys()) """
 
-    #print(app.config['JWT_SECRET_KEY'])
+        #print(app.config['JWT_SECRET_KEY'])
 
-    # Không khởi tạo ở create_app()
-    """     with app.app_context():
-        if not os.getenv("TESTING"):
-            db.drop_all()
-            db.create_all() """
-    # Kiểm tra endpoint
-    """ with app.app_context():
-        for rule in app.url_map.iter_rules():
-            methods = ','.join(rule.methods)
-            print(f"Endpoint: {rule.endpoint} | URL: {rule} | Methods: {methods}") """
+        # Không khởi tạo ở create_app()
+        """     with app.app_context():
+            if not os.getenv("TESTING"):
+                db.drop_all()
+                db.create_all() """
+        # Kiểm tra endpoint
+        """ with app.app_context():
+            for rule in app.url_map.iter_rules():
+                methods = ','.join(rule.methods)
+                print(f"Endpoint: {rule.endpoint} | URL: {rule} | Methods: {methods}") """
 
-    return app
+        return app
+    except Exception as e:
+            print("Error during app creation:", e)
+            import traceback
+            traceback.print_exc()
+            # raise lỗi ra ngoài để Gunicorn có thể biết
+            raise
 
 def setup_logging():
     if not os.path.exists("logs"):
@@ -75,3 +87,24 @@ def setup_logging():
     werkzeug_log = logging.getLogger('werkzeug')
     werkzeug_log.setLevel(logging.CRITICAL)
     #werkzeug_log.propagate = False 
+
+def wait_for_db(app, db, retries=5, delay=2):
+    last_exception = None
+
+    with app.app_context():
+        for i in range(retries):
+            try:
+                db.session.execute(text('SELECT 1'))  # test raw sql query
+                print("Database is ready!")
+                #db.create_all()
+                break
+            except OperationalError as e:
+                last_exception = e
+                print(f"Database not ready yet, retry {i + 1}/{retries}...")
+                time.sleep(delay)
+        else:
+            print("Database connection failed after retries. Exiting.")
+            if last_exception:
+                raise last_exception
+            else:
+                raise Exception("Database connection failed, but no exception captured.")
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 print("Main file loaded")
 
 
-from app import create_app, setup_logging
+import os
+from app import create_app, setup_logging, wait_for_db
 from app.config import Testing, Production, Config
+from app.extensions import db
 
 setup_logging()
 app = create_app(config_class=Production)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    wait_for_db(app, db) 
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=True)

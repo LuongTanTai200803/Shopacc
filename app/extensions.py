218fc6a41dfdbfa0aaffd3f7978292aaceeb2b87
khanh <123253526+Hello2k2@users.noreply.github.com>
2025-07-08 09:32:57 +0700

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -4,10 +4,11 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_caching import Cache
 from flask_socketio import SocketIO
-
+redis_url = os.environ.get('CACHE_REDIS_URL')
+print(f"DEBUG: Redis URL from environment is: {redis_url}")
 db = SQLAlchemy()
 jwt = JWTManager()
 migrate = Migrate() 
 cache = Cache()
-redis_url = os.environ.get('CACHE_REDIS_URL')
+
 socketio = SocketIO(async_mode='gevent', message_queue=redis_url)

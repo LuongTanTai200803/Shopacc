453a16c247d5a20aeb8ad42b497069f58bc6676c
Tai Dev <tantailg2003@gmail.com>
2025-07-08 14:34:32 +0700

    fix: start setup_logging before running app

--- start.sh
+++ start.sh
@@ -2,16 +2,20 @@
 
 set -e  # Dừng script nếu có bất kỳ lỗi nào
 
+echo "🛠️ Cấu hình logging..."
+python scripts/setup_logging.py
+
 echo "🚀 Khởi động Gunicorn server..."
 exec gunicorn wsgi:app -c gunicorn.conf.py
-
-echo "⏳ Đợi DB sẵn sàng..."
-python scripts/wait_for_db.py
 
 echo "🧱 Chạy Alembic migrations..."
 python scripts/upgrade_db.py
 
-echo "🛠️ Cấu hình logging..."
-python scripts/setup_logging.py
+echo "⏳ Đợi DB sẵn sàng..."
+python scripts/wait_for_db.py
+
+
+
+
 
 
